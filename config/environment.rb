1247c0397c9d204f2684c65b18dd7935b9e46f44
rogerdpack <rogerpack2005@gmail.com>
2010-11-23 08:36:45 -0700

    bump environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 require File.join(File.dirname(__FILE__), '../vendor/plugins/engines/boot')
@@ -17,11 +18,30 @@ Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here
   config.load_paths += %W( #{RAILS_ROOT}/vendor/plugins/substruct )
   config.action_controller.session_store = :active_record_store
+
+  # It seems Rack 1.1.0 SPECIFICALLY is required by Rails 2.3.8
+  config.gem "rack", :version => '1.1.0'
   
-  config.gem 'RedCloth'
+  config.gem 'RedCloth', :lib => 'redcloth'
   config.gem 'fastercsv' if RUBY_VERSION < '1.9.0'
   config.gem 'mime-types', :lib => 'mime/types'
   config.gem 'mini_magick', :version => '1.3.3'
   config.gem 'ezcrypto'
   config.gem 'subexec'
+  
+  # All of these gems are just so we can attach inline-css styles
+  # to the order receipt HTML email! :(
+  config.gem 'css_parser'
+  config.gem 'text-hyphen', :lib => 'text/hyphen'
+  config.gem 'text-reform', :lib => 'text/reform'
+  config.gem 'htmlentities'
+  # http://github.com/SunDawg/premailer
+  config.gem 'sundawg_premailer', :lib => 'premailer'
+  
+end
+
+if RUBY_VERSION >= '1.9.0'
+  # use the new CSV for CSV generation...
+  require 'csv'
+  FasterCSV = CSV
 end
