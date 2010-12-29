58bd1e2d92b928127a2e7eef2910d182d1cef62e
rogerdpack <rogerpack2005@gmail.com>
2010-12-29 11:54:03 -0700

    add migration...kind of

---
+++ db/migrate/001_add_comments.rb
@@ -0,0 +1,13 @@
+class AddComments < ActiveRecord::Migration
+def self.up
+    create_table :comments do |t|
+      t.column :product_id, :int
+      t.column :comment, :string
+    end
+    add_index "comments", ["product_id"], :name => "pid" # why not?
+  end
+  
+  def self.down
+    drop_table :comments
+  end
+end
