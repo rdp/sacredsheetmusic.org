1f554cceb8ec48678dc70dee578fd6b3d7357b73
rogerdpack <rogerpack2005@gmail.com>
2010-12-28 20:56:07 -0700

    tweaks

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -18,6 +18,32 @@ class StoreController < ApplicationController
       render(:file => "#{RAILS_ROOT}/public/404.html", :status => 404) and return
     end
   end
-  
+
+  # Our simple store index
+  def index
+    @title = "Songs"
+    respond_to do |format|
+      format.html do
+        @tags = Tag.find_alpha
+        @tag_names = nil
+        @viewing_tags = nil
+        @products = Product.paginate(
+          :order => 'name ASC',
+          :conditions => Product::CONDITIONS_AVAILABLE,
+          :page => params[:page],
+          :per_page => @@per_page
+        )
+        render :action => 'index.rhtml' and return
+      end
+      format.rss do
+        @products = Product.find(
+          :all,
+          :conditions => Product::CONDITIONS_AVAILABLE,
+          :order => "date_available DESC"
+        )
+        render :action => 'index.rxml', :layout => false and return
+      end
+    end
+  end 
   
 end
