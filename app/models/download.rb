9e63b9990718c87ffff514995bbca4731ac89b38
rogerdpack <rogerpack2005@gmail.com>
2010-12-30 13:13:43 -0700

    allow for larger uploads

--- app/models/download.rb
+++ app/models/download.rb
@@ -2,8 +2,18 @@ require_dependency RAILS_ROOT + "/vendor/plugins/substruct/app/models/download"
 
 class Download < UserUpload
 
+  MAX_SIZE = 120.megabyte
+
   def relative_path_to_web_server
     full_filename.gsub(RAILS_ROOT + "/public", "")
   end
+
+  has_attachment(
+    :storage => :file_system,
+    :max_size => MAX_SIZE,
+    :thumbnails => { :thumb => '50x50>', :small => '200x200' }, # only applies to images anyway, so we're good there
+    :processor => 'MiniMagick',
+    :path_prefix => 'public/system/'
+  )
 
 end
