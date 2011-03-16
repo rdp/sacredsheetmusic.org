efbfc9b125f2bed3b22a1ef771c9c3ffe94a4919
rogerdpack <rogerpack2005@gmail.com>
2011-03-16 11:14:47 -0600

    attempt

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,15 @@ class Product < Item
   has_many :images,
     :through => :product_images, :order => "-product_images.rank DESC",
     :dependent => :destroy
-    
+  
+  #
+  # the arranger tag for this product...if there is one...
+  def composer
+   self.tags.select{|t| t.parent && t.parent.name =~ /composer/i }[0]
+  end
+  
   def composer_contact
-     owner = self.tags.select{|t| t.parent && t.parent.name =~ /composer/i }[0]
+     owner = composer
      cc = (owner && owner.composer_contact.present? ) ? owner.composer_contact : nil
      cc = "mailto:" + cc if cc =~ /.@./
      cc
@@ -21,5 +27,23 @@ class Product < Item
       tags << Tag.find(id) if !id.to_s.empty?
     end
   end
-  
+
+  # Inserts code from product name if not entered.
+  # Makes code safe for URL usage.
+  def clean_code
+    if self.code.blank?
+      if self.composer
+        self.code = self.composer.name # composer full name?
+      else
+        self.code = self.name.clone 
+      end
+    end
+    self.code.upcase!
+    self.code = self.code.gsub(/[^[:alnum:]]/,'-').gsub(/-{2,}/,'-')
+    self.code = self.code.gsub(/^[-]/,'').gsub(/[-]$/,'')
+    self.code.strip!
+    return true
+	end
+
+
 end
