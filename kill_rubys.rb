c94ba11138c97a50bf6c7e1c7ded95d2aee4ce47
rogerdpack <rogerpack2005@gmail.com>
2011-04-30 20:59:50 -0600

    embed wave right

--- kill_rubys.rb
+++ kill_rubys.rb
@@ -1,9 +1,9 @@
 out = `ps -ef | grep ruby | grep wilkboar | grep dispatch`
-for line in out
+out.each_line{|line|
   # line === wilkboar 13785 25295  0 13:13 ?        00:00:01 /usr/bin/ruby dispatch.fcgi
   bad_pid = line.split(' ')[1]
   death_command = "kill #{bad_pid}"
   print death_command.inspect, "\n"
   system(death_command)
-end
+}
 
