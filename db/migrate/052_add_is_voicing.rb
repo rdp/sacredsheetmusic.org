b9d366907350d611ada1e7f9742ad34d72337a80
rogerdpack <rogerpack2005@gmail.com>
2011-07-05 17:21:27 -0600

    add

---
+++ db/migrate/052_add_is_voicing.rb
@@ -0,0 +1,11 @@
+class AddIsVoicing < ActiveRecord::Migration
+
+  def self.up
+    add_column :tags, :is_voicing, :boolean
+  end
+
+  def self.down
+    remove_column :tags, :is_voicing
+  end
+  
+end
