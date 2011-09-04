c499462b3981955485cdc6baea5abb9cc1873c26
rogerdpack <rogerpack2005@gmail.com>
2011-09-03 22:16:43 -0600

    beter link

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   
   def user_name_with_url_and_colon
     return '' unless user_name && user_name.length > 0
-    if user_url
+    if user_url.present?
       "<a href=\"#{user_url}\">" + user_name + "</a>:"
     else
       user_name + ":"
