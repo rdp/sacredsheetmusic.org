f2b22f5c6a4d193b2e7c814afe75ee99e1baaf20
rogerdpack <rogerpack2005@gmail.com>
2011-11-03 13:47:48 -0600

    add link

--- snapshot.sh
+++ snapshot.sh
@@ -1,4 +1,6 @@
+ruby script/runner -e production "Cache.clear!; Session.delete_all"
 echo m4m
 A=`date`
 B=`echo $A | tr -d \\n`
 mysqldump -uwilkboar_m4m wilkboar_m4m -p > "snap$B.sql"
+gzip "snap$B.sql"
