c6e5ee8811f9f6e1f7acf5190ad8750549052854
rogerdpack <rogerpack2005@gmail.com>
2012-01-03 10:20:39 -0700

    clarify

--- app/models/product.rb
+++ app/models/product.rb
@@ -122,7 +122,7 @@ class Product < Item
       end
       if !self.hymn_tag && (t = Tag.find_by_name(self.name) )
          unless t.id.in? self.tag_ids
-           problems << "song probably should be tagged with hymn name, or topic"
+           problems << "song probably should be tagged with hymn name, or topic [there is a tag that matches its title]"
          end
       end
       for tag in self.tags
