2c99fa4fe42c0312bbec39f81555db9ec3c3d1fb
rogerdpack <rogerpack2005@gmail.com>
2012-01-19 12:43:23 -0700

    not yet

--- app/models/cache.rb
+++ app/models/cache.rb
@@ -13,10 +13,6 @@ class Cache < ActiveRecord::Base
     get_or_set_int( collection.map{|record| [record, record.attributes]}, identifier) { yield }
   end
 
-  def self.get_or_set_ints(ints, identifier)
-
-  end
-
   def self.get_or_set_int(int, identifier)
     hash = [int, identifier].hash
     if a = Cache.find_by_hash_key(hash)
