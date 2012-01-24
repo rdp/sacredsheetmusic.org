d502d049bee51d8dacd095a602bf7a2c630aab76
rogerdpack <rogerpack2005@gmail.com>
2012-01-24 01:27:19 -0700

    not too much pain, 3s compared to 15 I guess

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,7 +9,7 @@ class Admin::ProductsController < Admin::BaseController
     @products = Product.paginate(
     :order => "name ASC",
     :page => params[:page],
-    :per_page => params[:per_page] || 100,
+    :per_page => params[:per_page] || 20,
     :include => [:tags, :downloads] # just fer fun...
     )
   end
