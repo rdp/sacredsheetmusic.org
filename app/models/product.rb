1dc3cd9de4f5a4d2acbd6fb0cd4a847ed2076753
rogerdpack <rogerpack2005@gmail.com>
2012-03-29 11:55:09 -0600

    good, working

--- app/models/product.rb
+++ app/models/product.rb
@@ -185,12 +185,12 @@ class Product < Item
         problems << "original url should start with http://"
       end
 
-      #topic_tags = Tag.find_by_name( "Topics", :include => :children).children
-
-      for topic_tag in Tag.find(:all)
+      topic_tags = Tag.find_by_name( "Topics", :include => :children).children
+      instrument_tags = Tag.find_by_name("Instrumental", :include => :children).children
+      for topic_tag in topic_tags + instrument_tags
         next if topic_tag.name.in? ['Christ', 'Work', 'Music'] # too common :)
         for topic_tag_name in topic_tag.name.split('/')
-          name_reg =  Regexp.new(Regexp.escape(topic_tag_name.strip), Regexp::IGNORECASE)
+          name_reg =  Regexp.new(Regexp.escape(topic_tag_name.strip) + "\\W", Regexp::IGNORECASE)
           if (self.name =~ name_reg) || (self.description =~ name_reg)
             if !self.tags.detect{|t| t.id == topic_tag.id}
               problems << "probably wants the #{topic_tag.name} tag, since its name is included in the title or description"
