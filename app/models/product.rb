832b7d7e152eb237918ad09974559cde7b485dd1
rogerdpack <rogerpack2005@gmail.com>
2012-03-31 12:07:08 -0600

    allow for non description

--- app/models/product.rb
+++ app/models/product.rb
@@ -191,7 +191,7 @@ class Product < Item
         next if topic_tag.name.in? ['Christ', 'Work', 'Music'] # too common :)
         for topic_tag_name in topic_tag.name.split('/')
           name_reg =  Regexp.new("\\W" + Regexp.escape(topic_tag_name.strip) + "\\W", Regexp::IGNORECASE)
-          if (self.name =~ name_reg) || (self.description.gsub('font-family', '') =~ name_reg)
+          if (self.name =~ name_reg) || (self.description.andand.gsub('font-family', '') =~ name_reg)
             if !self.tags.detect{|t| t.id == topic_tag.id}
               problems << "probably wants the #{topic_tag.name} tag, since its name is included in the title or description"
             end
