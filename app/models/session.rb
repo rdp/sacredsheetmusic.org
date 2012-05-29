0e20705dc80124ab044636f91d5af84bbffeddc8
rogerdpack <rogerpack2005@gmail.com>
2012-05-29 08:36:52 -0600

    add wishlist basics

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,11 @@
 require_dependency RAILS_ROOT + "/vendor/plugins/substruct/app/models/session"
 
 class Session
+
+  has_many :wishlist_items,
+    :dependent => :destroy,
+    :order => "created_on DESC"
+
   def self.clear!
    delete_all # skips validation...
   end  
---
+++ db/migrate/060_wishlist_to_session.rb
@@ -0,0 +1,9 @@
+class WishlistToSession < ActiveRecord::Migration
+def self.up
+    rename_column :wishlist_items, :order_user_id, :session_id
+  end
+  
+  def self.down
+   raise 'huh?'
+  end
+end
