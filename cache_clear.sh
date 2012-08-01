5bf9f3482641f58d4453484ff4ddda458a331f66
rogerdpack <rogerpack2005@gmail.com>
2012-08-01 07:08:49 -0600

    run after

--- cache_clear.sh
+++ cache_clear.sh
@@ -1,7 +1,7 @@
 echo 'clearing just cache, not sessions'
 kill
-echo 'please run kill'
 rm db/sqlite3.production
 ruby script/runner -e production db/migrate/dis.053_create_sqlite3_cache.rb 
 #ruby script/runner -e production "p Cache.clear!;" # just in case :P
+echo 'please run kill now'
 
