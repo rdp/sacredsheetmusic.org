169644964842ecdf7a6b5fdd9f8e1e20d235b17a
rogerdpack <rogerpack2005@gmail.com>
2012-11-02 08:23:59 -0600

    maybe better title for now

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -323,7 +323,7 @@ class MusicController < StoreController
     #viewing_tag_names = tag_names.join(" > ")
     original_size = all_products.size
     t = @viewing_tags[0]
-    @title = t.is_hymn_tag? ? t.name + " Sheet Music (#{original_size} Free Arrangements)" : t.name + " (Free LDS Sheet Music -- #{original_size} Arrangements)" 
+    @title = t.is_hymn_tag? ? t.name + " (#{original_size} Free Sheet Music Arrangements)" : t.name + " (Free LDS Sheet Music -- #{original_size} Arrangements)" 
     @products = paginate_and_filter(all_products)
 
     if @viewing_tags[0].bio
