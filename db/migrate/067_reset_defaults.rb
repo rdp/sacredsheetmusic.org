664fb3ecb83ee2f6d6563eb121b57574b20055a3
rogerdpack <rogerpack2005@gmail.com>
2013-01-26 22:03:05 -0700

    wurx

--- db/migrate/067_reset_defaults.rb
+++ db/migrate/067_reset_defaults.rb
@@ -3,11 +3,12 @@ class ResetDefaults < ActiveRecord::Migration
   def self.up
     change_column :user_uploads, :count, :int, :default => 0
     change_column :items, :view_count, :int, :default => 0
-    UserUploads.all.each{|uu| uu.count = uu.count-1; uu.save}
-    Items.all.each{|i| i.count = i.count -1; i.save}
+    UserUpload.all.each{|uu| uu.update_attribute(:count, uu.count-1)} # avoid redoing mogrify's with a full save...
+    Item.all.each{|i| Item.update_all({:view_count, i.view_count-1}, :id => i.id)} # avoid needing sane gem for full save
   end
 
   def self.down
+    #raise 'huh'
   end
   
 end
