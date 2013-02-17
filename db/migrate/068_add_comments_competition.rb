e72f9ef0c228b04244dda401f3b58cd4039c9acc
rogerdpack <rogerpack2005@gmail.com>
2013-02-16 20:58:40 -0700

    add columns

--- db/migrate/068_add_comments_competition.rb
+++ db/migrate/068_add_comments_competition.rb
@@ -2,10 +2,14 @@ class AddCommentsCompetition < ActiveRecord::Migration
 
   def self.up
     add_column :comments, :is_competition, :boolean, :default => false
+    add_column :comments, :created_at, :datetime
+    add_column :comments, :created_ip, :string
   end
 
   def self.down
     remove_column :comments, :is_competition
+    remove_column :comments, :created_at
+    remove_column :comments, :created_ip
   end
   
 end
