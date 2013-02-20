8c27e39b0eb2ff0c6ef994fd4216fec0a8809ac2
rogerdpack <rogerpack2005@gmail.com>
2013-02-19 20:51:34 -0700

    tweaks

--- lib/substruct.rb
+++ lib/substruct.rb
@@ -121,6 +121,7 @@ module Substruct
 
     # easier logout :)
     map.connect '/logout', :controller => "/accounts", :action=>"logout"
+    map.connect '/Competition', :controller => "/music", :action=>"competition"
 
     # Install the default route as the lowest priority.
     map.connect ':controller/:action/:id.:format'
