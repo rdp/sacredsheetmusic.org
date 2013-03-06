2841d39ab3cf72feef8051df6407c114ca88676c
rogerdpack <rogerpack2005@gmail.com>
2013-03-06 12:22:10 -0700

    less new look

--- cache_clear.sh
+++ cache_clear.sh
@@ -1,4 +1,5 @@
-echo 'clearing just cache, not sessions'
+echo 'clearing just cache and public/cache/*, not sessions'
+sleep 2
 rm public/cache/*
 ruby script/runner -e production "p Cache.clear!;" # just in case :P
 rm public/cache/*
