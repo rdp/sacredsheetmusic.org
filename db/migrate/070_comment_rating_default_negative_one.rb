282e06f66b87da2e913a090f42be95ae56889abf
rogerdpack <rogerpack2005@gmail.com>
2013-03-08 12:33:12 -0700

    add

--- db/migrate/070_comment_rating_default_negative_one.rb
+++ db/migrate/070_comment_rating_default_negative_one.rb
@@ -1,7 +1,8 @@
-class ResetDefaults < ActiveRecord::Migration
+class CommentRatingDefaultNegativeOne < ActiveRecord::Migration
 
   def self.up
     change_column :comments, :overall_rating, :int, :default => -1
+    Comment.all.select{|c| c.overall_rating == nil}.each{|c| c.overall_rating = -1; c.save}
   end
 
   def self.down
