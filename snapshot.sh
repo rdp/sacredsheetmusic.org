67aa99833f9cbe57725a05524ea50c5629294fcb
rogerdpack <rogerpack2005@gmail.com>
2013-06-17 17:32:30 +0000

    snap

--- snapshot.sh
+++ snapshot.sh
@@ -1,7 +1,8 @@
+echo "snapshotting production [broke]"
 ./sessions_clear.sh
 #./cache_clear.sh
 #rm log/* # we don't zip files anymore FWIW...
-echo m4m
+echo old password was m4m
 A=`date`
 B=`echo $A | tr -d \\n`
 mysqldump -uwilkboar_m4m wilkboar_m4m -p > "snap$B.sql"
