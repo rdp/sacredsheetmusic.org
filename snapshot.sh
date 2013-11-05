872227da2a268db1d67cfeb002c45b27d5b9e241
rogerdpack <rogerpack2005@gmail.com>
2013-11-05 21:31:21 +0000

    clear cache, much smaller, like, crazy small now

--- snapshot.sh
+++ snapshot.sh
@@ -1,11 +1,11 @@
 echo "snapshotting production"
 ./sessions_clear.sh
-#./cache_clear.sh
+./cache_clear.sh
 # partial clear instead, except it zips so well, who cares?
 #ruby script/runner -e production "p Cache.clear!" 
 #rm log/* # we don't zip files anymore FWIW...
 cat config/database.yml
-echo old password was m4m, enter new
+echo old password was m4m, but enter new
 A=`date`
 B=`echo $A | tr -d \\n`
 mysqldump -uprod_lds flds_production -p > "snap$B.sql"
