ed7bd92c1ffca8f8851fa92c30cbe27a3a253ca2
rogerdpack <rogerpack2005@gmail.com>
2013-11-22 14:31:17 +0000

    yo

--- app/controllers/youtube_edl_controller.rb
+++ app/controllers/youtube_edl_controller.rb
@@ -6,7 +6,10 @@ $template = ERB.new File.read(RAILS_ROOT + "/app/views/youtube_edl/control_youtu
 def combine_arrays array1, array2
     array1 ||= []
     array2 ||= []
-    raise "mismatch start with end count" unless array1.length == array2.length
+    # accomodate for a single start/end time [?]
+    array1 = [array1] unless array1.is_a? Array
+    array2 = [array2] unless array2.is_a? Array
+    raise "mismatch start with end count #{array1} #{array2}" unless array1.length == array2.length
     out = []
     array1.each_with_index{|start, idx|
       start = translate_string_to_seconds start
@@ -23,7 +26,7 @@ def yo
     mutes = combine_arrays incoming_params['mute_start'], incoming_params['mute_end']
     splits = combine_arrays incoming_params['skip_start'], incoming_params['skip_end']
     video_id = incoming_params['youtube_video_id'][0]
-    should_loop = incoming_params['loop'][0] || '0' # everything's an array, even if not there? weird
+    should_loop = incoming_params['loop'] == '1' ? '0' : '0'
     out = $template.result(binding)
     # html tag has already been closed...hmm
     out +=  'mutes: ' + mutes.join(', ') + ' skips: ' + splits.join(', ') + "\n"
