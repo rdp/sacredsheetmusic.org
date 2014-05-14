a9e0e5820e836fc70e9b3b2d5b6b1e630fbc9820
rogerdpack <rogerpack2005@gmail.com>
2014-05-14 22:04:47 +0000

    pretend to have less invasive snapshots

--- snapshot.sh
+++ snapshot.sh
@@ -2,12 +2,12 @@ echo "snapshotting production"
 ./sessions_clear.sh
 #./cache_clear.sh
 # partial clear instead, except it zips so well, who cares?
-ruby script/runner -e production "p Cache.clear!" 
+nice ruby script/runner -e production "p Cache.clear!" 
 #rm log/* # we don't zip files anymore FWIW...
 cat config/database.yml
 echo old password was m4m, but enter new, above
 A=`date`
 B=`echo $A | tr -d \\n`
-mysqldump -uprod_lds flds_production -p > "snap$B.sql"
+nice mysqldump -uprod_lds flds_production -p > "snap$B.sql"
 gzip "snap$B.sql"
 echo "not snapping pub files..."
