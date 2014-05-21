2afea46bbfc1749d8f4de8daf1ec3e3852a8bc41
rogerdpack <rogerpack2005@gmail.com>
2014-05-21 17:46:09 +0000

    better maybe

--- cache_clear.sh
+++ cache_clear.sh
@@ -3,6 +3,4 @@ sleep 2
 rm public/cache/*
 ruby script/runner -e production 'p Cache.clear!;' # just in case :P
 rm public/cache/*
-touch tmp/restart.txt
-echo 'should be restarting'
-
+./restart.sh
