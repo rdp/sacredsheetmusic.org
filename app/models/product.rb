602c132b4639520594ab9d9eeeeaca5c1d24813b
rogerdpack <rogerpack2005@gmail.com>
2014-09-05 12:44:21 +0000

    maybe humanify

--- app/models/product.rb
+++ app/models/product.rb
@@ -128,7 +128,7 @@ class Product < Item
           voicing_name = voicing_name.split(/ or /i)[0] # prefer "Youth Choir" from "youth choir or..."
           self.code = self.name.clone + '-' + voicing_name + '-by-' + self.composer_tag.name
         else
-          raise 'please setup voicing tags first (use back button on browser) or manually enter a product code for it'
+          raise 'please check some voicing options first (use back button on browser to proceed) (if no voicing options match, please tell us!)'
         end
       else
         raise 'please select a composer first (use back button on browser)--no composer selected!'
