feb6b4b3f558a91c4547d025b8e5c18e49cf7c3f
rogerdpack <rogerpack2005@gmail.com>
2014-12-10 14:02:02 +0000

    crampify the top

--- cache_clear.sh
+++ cache_clear.sh
@@ -1,4 +1,5 @@
 echo 'clearing cache and public/cache/* [whole kit and kaboodle], not clearing sessions'
+echo 'dont do this with session clear too close...'
 sleep 2
 rm public/cache/*
 ruby script/runner -e production 'p Cache.clear!;' # just in case :P
