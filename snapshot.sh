f65007894fdbcc903cd4e06b53910ea8ab014cdf
rogerdpack <rogerpack2005@gmail.com>
2015-05-07 20:13:53 +0000

    supah fast db update...hmm...except sessions have bookmarks tho...hmm

--- snapshot.sh
+++ snapshot.sh
@@ -1,5 +1,5 @@
-echo "snapshotting production"
-./sessions_clear.sh
+echo "snapshotting (production) DB..."
+#./sessions_clear.sh
 #./cache_clear.sh
 # partial clear instead, except it zips so well, who cares?
 nice ruby script/runner -e production "p Cache.clear!" 
@@ -8,6 +8,8 @@ cat config/database.yml
 echo old password was m4m, but enter new, above
 A=`date`
 B=`echo $A | tr -d \\n`
-nice mysqldump -uprod_lds flds_production -p > "snap$B.sql"
+# allow this to lock the tables since it 3s total anyway...
+nice mysqldump -uprod_lds flds_production -pprod_lds_pass --ignore-table=flds_production.sessions --ignore-table=flds_production.cache > "snap$B.sql"
 gzip "snap$B.sql"
+echo "created snap$B.sql"
 echo "not snapping pub files..."
