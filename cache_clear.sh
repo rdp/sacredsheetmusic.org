27b22432bed560155818eafcbbece085b28691c1
rogerdpack <rogerpack2005@gmail.com>
2015-05-11 16:28:54 +0000

    cleanup script

--- cache_clear.sh
+++ cache_clear.sh
@@ -1,7 +1,6 @@
 echo 'clearing cache and public/cache/* [whole kit and kaboodle], not clearing sessions'
 echo 'dont do this with session clear too close... (locks table?)'
-sleep 2
 rm public/cache/*
-ruby script/runner -e production 'p Cache.clear!;' # just in case :P
+ruby script/runner -e production 'p Cache.clear!;' # just in case, also clear internal html caches
 rm public/cache/*
 ./restart.sh
--- snapshot.sh
+++ snapshot.sh
@@ -2,15 +2,14 @@ echo "snapshotting (production) DB..."
 #./sessions_clear.sh
 #./cache_clear.sh
 # partial [non file] clear instead, except it zips so well, who cares?
-puts "clearing cache table"
+echo "clearing cache table(s) [only, partial cache clear]"
 nice ruby script/runner -e production "p Cache.clear!" 
-#rm log/* # we don't zip files anymore FWIW...
-cat config/database.yml
-echo old password was m4m, but enter new, above
+#rm log/* # we don't zip files anymore FWIW, so don't need to remot these...
 A=`date`
 B=`echo $A | tr -d \\n`
 # allow this to lock the tables since it 3s total anyway...
-nice mysqldump -uprod_lds flds_production -pprod_lds_pass --ignore-table=flds_production.sessions --ignore-table=flds_production.cache > "snap$B.sql"
+#mysqldump -uprod_lds flds_production -pprod_lds_pass --ignore-table=flds_production.sessions --ignore-table=flds_production.cache > "snap$B.sql"
+nice mysqldump -uprod_lds flds_production -pprod_lds_pass > "snap$B.sql"
 gzip "snap$B.sql"
 echo "created snap$B.sql"
 echo "not snapping pub files..."
