f7c671cc571cf8f950fa1279763a7fa6dde4b061
rogerdpack <rogerpack2005@gmail.com>
2015-07-10 17:21:28 -0400

    substruct friendly! LOL

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -28,15 +28,13 @@ class AccountsController < ApplicationController
   end
 
   def new_song_editor
-    Rails.logger.info params.inspect
+    @title = "create new user to upload your songs"
     if session[:user]
       @user = User.find session[:user] # already logged in, so an update
     end
     if request.post?
       if !@user
-         Rails.logger.info("saving user #{params[:user]} all are #{User.all.map &:login}")
          @user = User.new(params[:user]) # disallow any failure for now :|
-         Rails.logger.info("saving user name #{@user.login} #{@user.inspect} all are #{User.all.map &:login}")
          @user.save!
       end 
       @user.update_attributes!(params["user"]) # implies a save, BTW
@@ -49,10 +47,11 @@ class AccountsController < ApplicationController
       @user.composer_tag = @composer_tag # in case an initial save
       product_editor = Role.find_by_name "Product Editor"
       @user.roles << product_editor unless @user.roles.contain?(product_editor)
-      # TODO send email so they can remember their login name?
-      flash[:notice] = 'Successfully created login, use it to login now'
-      Rails.logger.info "SUCCESS creating #{@user.login}"
-      redirect_to '/admin' and return # force them to use it to login now :)
+      # TODO send email so they can remember their login name? or make them remember it LOL
+      Rails.logger.info "SUCCESS creating #{@user.login} #{@composer_tag.name}"
+      flash[:notice] = "Successfully created login #{@user.login}, use it to login now"
+      redirect_to "/admin" # forces a login
+      return
     end
     # edit or new
     if @user
