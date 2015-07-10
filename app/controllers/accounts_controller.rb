7f76d9056d4fb2d3d3e5708e7b1a8a99efe2a5fe
rogerdpack <rogerpack2005@gmail.com>
2015-07-10 17:49:18 -0400

    get error messages to show up, ftw

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -31,41 +31,38 @@ class AccountsController < ApplicationController
     @title = "create new user to upload your songs"
     if session[:user]
       @user = User.find session[:user] # already logged in, so an update
-    end
-    if request.post?
-      if !@user
-         @user = User.new(params[:user]) # disallow any failure for now :|
-         @user.save!
-      end 
-      @user.update_attributes!(params["user"]) # implies a save, BTW
-      @composer_tag = @user.composer_tag || Tag.create!(params["composer_tag"])
-      @composer_tag.update_attributes(params["composer_tag"])
-      composers = Tag.find_by_name "Composers"
-      @composer_tag.parent = composers
-      @composer_tag.save!
-      composers.alphabetize_children!
-      @user.composer_tag = @composer_tag # in case an initial save
-      product_editor = Role.find_by_name "Product Editor"
-      @user.roles << product_editor unless @user.roles.contain?(product_editor)
-      # TODO send email so they can remember their login name? or make them remember it LOL
-      Rails.logger.info "SUCCESS creating #{@user.login} #{@composer_tag.name}"
-      flash[:notice] = "Successfully created login #{@user.login}, use it to login now"
-      redirect_to "/admin" # forces a login
-      return
-    end
-    # edit or new
-    if @user
       if @user.is_admin?
-        raise "admins should not use this"
+        raise "admins should not use this" # too dangerous since it messes with permissions :P
       end
       @composer_tag = @user.composer_tag
+      @user.password = @user.password_confirmation =  '' # show blank typically to start since these are md5's anyway at save time apparently gets replaced with an md5 equivalent. weird.
     else
-      # new login request...
       @user = User.new
       @composer_tag = Tag.new
     end
 
-    @user.password = @user.password_confirmation =  '' # show blank typically to start since these are md5's anyway
+    if request.post?
+      # update_attributes attempts a save, and sets local attribute values, and sets id. wow.
+      # TODO relookup composer tag? huh?
+      if @user.update_attributes(params["user"]) && @composer_tag.update_attributes(params["composer_tag"])
+        # assume the rest will all work LOL
+        composers = Tag.find_by_name "Composers"
+        @composer_tag.parent = composers
+        @composer_tag.save!
+        composers.alphabetize_children!
+        @user.composer_tag = @composer_tag # in case of an initial save...
+        @user.save!
+        product_editor = Role.find_by_name "Product Editor"
+        @user.roles << product_editor unless @user.roles.contain?(product_editor)
+        # TODO send email so they can remember their login name? or make them remember it LOL
+        Rails.logger.info "SUCCESS creating #{@user.login} #{@composer_tag.name}"
+        flash[:notice] = "Successfully created login #{@user.login}, use it to login now"
+        redirect_to "/admin" # forces a re-login
+        return
+      else
+        # fall through and render with failure messages
+      end
+    end
     render :layout => 'main_no_box_admin'
   end
 
