0107ec2ce50f2d56283e2f08714e507cb55e8ec6
rogerdpack <rogerpack2005@gmail.com>
2015-12-07 03:33:46 -0500

    add host here

--- lib/substruct.rb
+++ lib/substruct.rb
@@ -410,3 +410,16 @@ module ::Enumerable
     inject({}) {|h,v| h[v]=h[v].to_i+1; h}.reject{|k,v| v==1}.keys
   end
 end
+
+
+# host's IP yikes
+def local_ip
+  orig = Socket.do_not_reverse_lookup  
+  Socket.do_not_reverse_lookup =true # turn off reverse DNS resolution temporarily
+  UDPSocket.open do |s|
+    s.connect '64.233.187.99', 1 #google
+    s.addr.last
+  end
+ensure
+  Socket.do_not_reverse_lookup = orig
+end
