4c377c8c30194db7b5ed6e1b3619ffecfce340e7
rogerdpack <rogerpack2005@gmail.com>
2016-07-08 10:10:28 -0400

    attempt to avoid lame redir, didn't work sigh

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
-$: << '.' if RUBY_VERSION >= '1.9.0'
+$: << '.' if RUBY_VERSION >= '1.9.0' # not that we really allow 1.9 :|
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 #require File.dirname(__FILE__) + "/../lib/faster_require-0.9.2/lib/faster_require" # faster require speeds all around...
@@ -17,6 +17,7 @@ require File.join(File.dirname(__FILE__), '../vendor/plugins/engines/boot')
 
 #require the plugin loader, for threadsafe engines...
 require File.join(File.dirname(__FILE__), '..', 'lib', 'eager_loader')
+
 
 Rails::Initializer.run do |config|
   # Necessary for us to run legacy engine migrations
@@ -60,4 +61,6 @@ if RUBY_VERSION >= '1.9.0'
   FasterCSV = CSV
 end
 
+reuqire 'open-uri'
+require 'open_uri_allow_redirections_to_https' # my own gem vendorization LOL
 Substruct.override_ssl_production_mode = true
