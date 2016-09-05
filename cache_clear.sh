f9b360b50053c10f4b0526d021bbbba171721edb
rogerdpack <rogerpack2005@gmail.com>
2016-09-05 13:58:19 -0400

    maybe clarify :|

--- cache_clear.sh
+++ cache_clear.sh
@@ -1,6 +1,6 @@
 echo 'clearing cache and public/cache/* [whole kit and kaboodle], not clearing sessions'
-echo 'dont do this with session clear too close... (locks table?)'
+echo 'dont do this with session clear too close... (locks table?) anyway as the cache repopulates is a bad time to do this...'
 rm public/cache/*
-ruby script/runner -e production 'p Cache.clear!;' # just in case, also clear internal html caches
+ruby script/runner -e production 'p Cache.clear!;' # just in case, also clears internal html caches
 rm public/cache/*
 ./restart.sh
