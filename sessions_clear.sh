de649095ebeb7f819b52287a4d4bd81320739fd6
rogerdpack <rogerpack2005@gmail.com>
2016-09-05 14:09:06 -0400

    clarify

--- sessions_clear.sh
+++ sessions_clear.sh
@@ -3,6 +3,9 @@ ruby script/runner -e production "
 good_ids = {}
 WishlistItem.all.each{|wli| good_ids[wli.session_id] = 1}
 
+# delete_all loads nothing into memory
+# unfortunately this does appear to lock the whole sessions table, but at least its only for awhile :|
+# TODO iterate over each of them? gah...
 p Session.delete_all([\"id not in (?)\", good_ids.keys])
 p good_ids.size, 'sessions retained'
 "
