885408119486668e609afaae507c6578bf4e8864
rogerdpack <rogerpack2005@gmail.com>
2016-11-10 23:19:03 -0500

    get ready to memoize

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -695,10 +695,16 @@ class MusicController < StoreController
     )
 
     # allow searches like "christmas duet" to work...unclear how to do this in sql...
-    with_all_words_somewhere = Product.find(:all, :include => :tags, :conditions => Product::CONDITIONS_AVAILABLE, :order => session_rand).select{|p| 
-       big_string = (p.name.to_s + " " + p.description.to_s + " " + p.lyrics.to_s + " " + p.tags.map{|t| t.name + " " + t.bio.to_s}.join(" ")).downcase.split # split so mary doesn't match primary though some of this still occurs in the SQL query within description...
-       words_to_search_for.all?{|word| big_string.contain? word}
+    all_songs_with_tags = Product.find(:all, :include => :tags, :conditions => Product::CONDITIONS_AVAILABLE, :order => session_rand)
+    all_songs_with_strings = all_songs_with_tags.map{ |p| 
+       all_tags =  p.tags.map{|t| t.name + " " + t.bio.to_s }.join(" ")
+       big_string = (p.name.to_s + " " + p.description.to_s + " " + p.lyrics.to_s + " " + all_tags).downcase.split # split so mary doesn't match primary though some of this still occurs in the SQL query within description...
+       [p, big_string]
     }
+
+    with_all_words_somewhere = all_songs_with_strings.select{|p, big_string| 
+       words_to_search_for.all?{|word| big_string.contain? word}
+    }.map{|p, big_string| p}
 
     # search for all products of (basically) precise matching tags, too
     # this might be redundant to the above these days...
