ccd3b86ff17d51cc85307627f38ba8dd93dc688a
rogerdpack <rogerpack2005@gmail.com>
2016-11-21 17:58:06 -0500

    find_by_tag_id 4.4s => 2.3s

--- app/models/product.rb
+++ app/models/product.rb
@@ -539,7 +539,8 @@ class Product < Item
   end
 
   def self.find_by_tag_id(tag_id, order_by = 'items.name ASC')
-    tag = Tag.find(tag_id, :include => [{:products => [:tags]}], :order => order_by) # 4.5s
+    tag = Tag.find(tag_id, :include => [{:products => [:tags]}]) 
+    tag.products.sort_by{|p| p.name} # == items.name ASC ... I think LOL but twice as fast, ai ai rails
     tag.songs
   end
 
