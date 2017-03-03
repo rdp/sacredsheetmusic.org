0534cb633b48a640ca2f63dec59b76306d083fc6
rogerdpack <rogerpack2005@gmail.com>
2017-03-03 12:44:25 -0500

    notes

--- snapshot.sh
+++ snapshot.sh
@@ -1,8 +1,8 @@
 echo "snapshotting (production) DB..."
-echo "this does not do a sessions nor cache clear, but it does do a half cache clear, first..."
-#./sessions_clear.sh
+echo "this does a sessions and half cache clear..."
+./sessions_clear.sh
 #./cache_clear.sh
-# partial [non file] clear instead, except it zips so well, who cares?
+# partial [non file] clear instead ... in case it brings down prod too hard [though prod theoretically recovers OK'ish better more now]
 echo "clearing cache table(s) [only, partial cache clear]"
 nice ruby script/runner -e production "p Cache.clear!" 
 #rm log/* # we don't zip files anymore FWIW, so don't need to remot these...
