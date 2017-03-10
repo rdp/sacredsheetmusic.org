bb3f5f22068e1a69c55b88793b0e063feae227d6
rogerdpack <rogerpack2005@gmail.com>
2017-03-10 12:58:53 -0500

    Page is mobile-friendly LOL

--- templates_clear.sh
+++ templates_clear.sh
@@ -1,5 +1,5 @@
 echo 'this is if the products have stayed the same, just the layout is different'
-rm public/cache/*
+rm public/cache*/*
 ./restart.sh
-rm public/cache/*
+rm public/cache*/*
 echo 'done [one of the warnings above is expectedish]'
