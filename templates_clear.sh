013ac77c0d9738da2d6e58d3cf9d2cc23639ab15
rogerdpack <rogerpack2005@gmail.com>
2017-03-13 12:54:58 -0400

    yep

--- templates_clear.sh
+++ templates_clear.sh
@@ -1,4 +1,4 @@
-echo 'this is if the products have stayed the same, just the layout is different'
+echo 'this is if the products have stayed the same, just the non product layout has changed'
 rm `find public/cache*/* | grep -v git`
 ./restart.sh
 rm `find public/cache*/* | grep -v git`
