80a0cc9c09bf589a3ef4683ab4cc48afaec8f849
rogerdpack <rogerpack2005@gmail.com>
2017-07-10 14:25:50 -0400

    clear betuh

--- cache_clear.sh
+++ cache_clear.sh
@@ -1,7 +1,10 @@
 echo 'clearing cache and public/cache/* [whole kit and kaboodle], not clearing sessions'
 echo 'dont do this with session clear too close... (locks table?) anyway as the cache repopulates is a bad time to do this...'
 rm public/cache/*
+rm public/song_cache_square/*
 ruby script/runner -e production 'p Cache.clear!;' # just in case, also clears internal html caches
 rm public/cache/*
+rm public/song_cache_square/*
 touch public/cache/git_keep_dir
+touch public/song_cache_square/git_marker
 ./restart.sh
