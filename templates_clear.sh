4f6b31a193302994d9b66453a260776a0526c9cf
rogerdpack <rogerpack2005@gmail.com>
2017-12-03 23:00:05 -0500

    moh

--- templates_clear.sh
+++ templates_clear.sh
@@ -1,5 +1,6 @@
 echo 'this is if the products have stayed the same, just the non product layout has changed'
 rm `find public/cache*/* | grep -v git`
-./restart.sh
+ruby script/runner -e production 'p Cache.clear!;' # just in case, also clears internal html caches...
+./restart.sh # probably redundant
 rm `find public/cache*/* | grep -v git`
 echo 'done [one of the warnings above is expectedish]'
